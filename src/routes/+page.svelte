f1b5328f89a06ede2456cdc95a2721337e466e24
KevinITHS <kevin.ali@iths.se>
2023-08-23 13:43:36 +0200

    structure/format

---
+++ src/routes/+page.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+import Component1 from '$lib/components/Component1.svelte';
+</script>
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<Component1/>
